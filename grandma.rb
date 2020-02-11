9db05bddccf269fe450d5e059884f05256ae9ff5
Pablo Gonzalez <pabgon0@gmail.com>
2020-02-11 17:07:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 def speak_to_grandma(phrase)
-  
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == .downcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else phrase == .upcase
+    puts "NO, NOT SINCE 1938!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
