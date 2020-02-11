5ca6337e5c306c8b6e832d1f24a7e8623a7b07ea
Pablo Gonzalez <pabgon0@gmail.com>
2020-02-11 17:13:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
