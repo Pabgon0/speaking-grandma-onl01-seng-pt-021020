2b78cde3a332cfbb4392b1a4a66c3d33653ac1d7
Pablo Gonzalez <pabgon0@gmail.com>
2020-02-11 17:09:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   else phrase == .upcase
     puts "NO, NOT SINCE 1938!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
